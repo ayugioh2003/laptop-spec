0b61baf9c2ea6d7ad467e7096b1a9e5f49c9e848
John Chiu <ayugioh2003@gmail.com>
2025-07-15 00:49:49 +0800

    fix: update package.json and vite.config.js for module support and path resolution

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
-const resolve = (dir) => path.resolve(__dirname, dir);
+const resolve = (dir) => path.resolve(path.dirname(fileURLToPath(import.meta.url)), dir);
 
 // https://vitejs.dev/config/
 // https://github.com/zhaojianzz/elementPlus-manage-system/blob/main/vite.config.ts
 export default defineConfig({
   plugins: [vue()],
-  alias: [
-    { find: '@', replacement: resolve('src') },
-    { find: '@assets', replacement: resolve('src/assets') },
-    { find: '@components', replacement: resolve('src/components') },
-    { find: '@hooks', replacement: resolve('src/hooks') },
-    { find: '@utils', replacement: resolve('src/utils') },
-  ],
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve('src') },
+      { find: '@assets', replacement: resolve('src/assets') },
+      { find: '@components', replacement: resolve('src/components') },
+      { find: '@hooks', replacement: resolve('src/hooks') },
+      { find: '@utils', replacement: resolve('src/utils') },
+    ],
+  },
   // https://blog.csdn.net/qq_17497931/article/details/109510796
   base: './',
 })
